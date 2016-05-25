0a2a9a16278ddb15fd89ecdb662aa540917af1c2
张单旸晖 <236294386@qq.com>
2016-05-25 22:25:06 +0800

    整理master分支下的代码缩进

--- src/com/zaizai/volume/MainActivity.java
+++ src/com/zaizai/volume/MainActivity.java
@@ -19,14 +19,12 @@ public class MainActivity extends Activity {
 
  
 
-	  private static final String TAG = "AudioRecord";
-	  static final int SAMPLE_RATE_IN_HZ = 8000;
-	  static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE_IN_HZ,
-	          AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
-	  AudioRecord mAudioRecord;
-	  boolean isGetVoiceRun;
-	  Object mLock;
-	
+	private static final String TAG = "AudioRecord";
+	static final int SAMPLE_RATE_IN_HZ = 8000;
+	static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLE_RATE_IN_HZ,AudioFormat.CHANNEL_IN_DEFAULT, AudioFormat.ENCODING_PCM_16BIT);
+	AudioRecord mAudioRecord;
+	boolean isGetVoiceRun;
+	Object mLock;
 
 	private TextView textView;
 	private Handler handler = new MyHandler();
@@ -41,16 +39,13 @@ public class MainActivity extends Activity {
 		mLock = new Object();//1
 		getNoiseLevel();//2
 		
-	
-		
  }        
 	     
 		
 		
 	
 	@Override
-	protected void onDestroy () 
-	{
+	protected void onDestroy () {
 		super.onDestroy(); 
         mAudioRecord.stop();
        // mAudioRecord.release();
@@ -58,50 +53,49 @@ public class MainActivity extends Activity {
   
       
 	
-	}
+}
 	
 	
 	
 	int i=0;
 	Queue<String> queue = new LinkedBlockingQueue<String>();
 	Bundle receiveBundle;
-	  class MyHandler extends Handler{
-		  	
-			@Override
-			public void handleMessage(Message msg) {
-				super.handleMessage(msg);
-				if(msg.what==1)
-				{
-					receiveBundle= msg.getData();
-					Double dtemp=receiveBundle.getDouble("volume");
-					String s=Double.toString(dtemp);
-					if(dtemp==0.0)
-					{
-						return;
-					}
+	class MyHandler extends Handler{
+		@Override
+		public void handleMessage(Message msg) {
+		super.handleMessage(msg);
+		if(msg.what==1)
+		{
+			receiveBundle= msg.getData();
+			Double dtemp=receiveBundle.getDouble("volume");
+			String s=Double.toString(dtemp);
+			if(dtemp==0.0)
+			{
+				return;
+			}
 					
-					s+='\n';
-					queue.offer(s);
-					i++;
-					String sall="";
-	                //textView.setText(s);
-					for(String x :queue)
-					{
-						sall+=x;
-					}
-					textView.setText(sall);
-	                if(i==35)
-	                {
-	                	queue.poll();
-	                	i--;
-	                }
-				}
+			s+='\n';
+			queue.offer(s);
+			i++;
+			String sall="";
+	        //textView.setText(s);
+			for(String x :queue)
+			{
+				sall+=x;
+			}
+			textView.setText(sall);
+	        if(i==35)
+	        {
+	        queue.poll();
+	        i--;
+	        }
+		}
 				
 				
 
-				}
+	}
                
-		}
+}
 	    
 	 
 	
@@ -112,25 +106,26 @@ public class MainActivity extends Activity {
 	      return;
 	    }
 	    mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
-	        SAMPLE_RATE_IN_HZ, AudioFormat.CHANNEL_IN_DEFAULT,
-	        AudioFormat.ENCODING_PCM_16BIT, BUFFER_SIZE);
+	    SAMPLE_RATE_IN_HZ, AudioFormat.CHANNEL_IN_DEFAULT,
+	    AudioFormat.ENCODING_PCM_16BIT, BUFFER_SIZE);
 	    if (mAudioRecord == null) {
 	      Log.e("sound", "mAudioRecord初始化失败");
-	    }
+}
+	    
+	    
 	    isGetVoiceRun = true;
 	 
-	    
-
 	    new Thread(new Runnable() {
 	      @Override
 	      public void run() {
 	    	  
-	    	 Bundle data = new Bundle(); //线程通信，传递数据
+	    	Bundle data = new Bundle(); //线程通信，传递数据
 	    	  
 	    	  
 	        mAudioRecord.startRecording();
 	        short[] buffer = new short[BUFFER_SIZE];
 	        while (isGetVoiceRun) {
+	        	
 	          //r是实际读取的数据长度，一般而言r会小于buffersize
 	          int r = mAudioRecord.read(buffer, 0, BUFFER_SIZE);
 	          long v = 0;
@@ -138,20 +133,17 @@ public class MainActivity extends Activity {
 	          for (int i = 0; i < buffer.length; i++) {
 	            v += buffer[i] * buffer[i];
 	          }
+	          
 	          // 平方和除以数据总长度，得到音量大小。
 	          double mean = v / (double) r;
 	          double volume = 10 * Math.log10(mean);
 	         
-	        	Message msg = handler.obtainMessage();//线程通信，传递数据
-	    		msg.what=1;
-	    		data.putDouble("volume", volume);; 
-	    		msg.setData(data); 
-	    		handler.sendMessage(msg);
-	    		
-	    		
+	          Message msg = handler.obtainMessage();//线程通信，传递数据
+	    	  msg.what=1;
+	    	  data.putDouble("volume", volume);; 
+	    	  msg.setData(data); 
+	    	  handler.sendMessage(msg);
 
-	          
-	        
 	         // Log.d(TAG, "分贝值:" + volume);
 	          
 	    		
@@ -164,9 +156,9 @@ public class MainActivity extends Activity {
 	            }
 	          }
 	        }
-	        mAudioRecord.stop();
-	        mAudioRecord.release();
-	        mAudioRecord = null;
+	          mAudioRecord.stop();
+	          mAudioRecord.release();
+	          mAudioRecord = null;
 	      }
 	    }).start();
 	  }
